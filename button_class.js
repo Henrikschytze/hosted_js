3e1966d87d53b69a2568dab6c9c20096e278583e
Henrik Schütze <33839074+Henrikschytze@users.noreply.github.com>
2021-04-08 10:19:23 +0200

    Create button_class.js

---
+++ button_class.js
@@ -0,0 +1,9 @@
+setTimeout(function(){ 
+  
+let elm = document.querySelectorAll("[data-button-for='332f95675f69206f48fdfa573a95153d12d4597d']");
+  if(elm && elm[0].style.opacity === "1") {
+    elm[0].classList.add('mopinion-button--floating')
+  }
+  
+
+}, 4000);
